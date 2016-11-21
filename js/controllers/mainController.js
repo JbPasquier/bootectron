249c892ea10c43ba10f03e3c9e013ede0234f9c4
Jean-Baptiste Pasquier <contact@jbpasquier.eu>
2016-11-21 11:19:17 +0100

    Indentation

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -2,5 +2,8 @@ function mainController(electron) {
     this.menu = ['Hello', 'Sweet', 'Home'];
     this.title = 'Hello, world!';
     this.description = "My name is AnguBootstrap";
-    this.alert = () => {electron.clipboard.writeText('Do you like bananas ?'); electron.dialog.showErrorBox('Hey dude', 'Message copié dans le presse papier')};
+    this.alert = () => {
+        electron.clipboard.writeText('Do you like bananas ?');
+        electron.dialog.showErrorBox('Hey dude', 'Message copié dans le presse papier');
+    };
 }
